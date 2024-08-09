814c29e948e5befea4e840e6db89300b07e8ae2e
jubair-jx <jubair.official97@gmail.com>
2024-08-10 01:22:56 +0600

    navbar and dashboard sidebar is updateing now

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <section className=" h-full relative">
+      <div className=" hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[90] bg-gray-900">
+        <h1 className=" font-semibold mt-2 text-4xl text-gray-100 font-mono text-center">
+          InnovaX
+        </h1>
+      </div>
+      <main className=" md:pl-80">{children}</main>
+    </section>
+  );
+};
+
+export default DashboardLayout;
