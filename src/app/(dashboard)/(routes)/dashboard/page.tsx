8a9f25121e1fd7447bdf748b88472dce0f1bb003
jubair-jx <jubair.official97@gmail.com>
2024-08-10 12:52:59 +0600

    sidebar is working now

--- src/app/(dashboard)/(routes)/dashboard/page.tsx
+++ src/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,12 +1,5 @@
-import { UserButton } from "@clerk/nextjs";
-
 const DashboardPage = () => {
-  return (
-    <div>
-      DashboardPage
-      <UserButton afterSwitchSessionUrl="/" />
-    </div>
-  );
+  return <div>DashboardPage</div>;
 };
 
 export default DashboardPage;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,18 @@
+import { UserButton } from "@clerk/nextjs";
+import { MenuIcon } from "lucide-react";
+import { Button } from "./ui/button";
+
 const Navbar = () => {
-  return <div className=" flex items-center">Button</div>;
+  return (
+    <div className=" flex items-center p-4">
+      <Button variant={"ghost"} size={"icon"} className=" md:hidden">
+        <MenuIcon />
+      </Button>
+      <div className=" flex justify-end w-full">
+        <UserButton afterSwitchSessionUrl="/" />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
