4c2d8828e637919761a94e9861c632d732326517
jubair-jx <jubair.official97@gmail.com>
2024-08-11 01:27:50 +0600

    dashboard page info is working now

--- src/app/(dashboard)/(routes)/dashboard/page.tsx
+++ src/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,17 @@
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  return (
+    <div>
+      <div className=" mb-8 space-y-4">
+        <h2 className=" text-2xl md:text-5xl font-bold text-center">
+          Explore the journey of AI
+        </h2>
+        <p className=" text-muted-foreground font-light text-sm md:text-lg text-center">
+          Meet and talk about the future of ai
+        </p>
+      </div>
+      <div className=" px-4 md:px20 lg:px-32 space-y-4">{}</div>
+    </div>
+  );
 };
 
 export default DashboardPage;
